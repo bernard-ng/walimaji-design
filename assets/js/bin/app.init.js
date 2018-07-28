68916183264acc239264ce980224426c46d4a519
bernard-ng <ngandubernard@gmail.com>
2018-07-28 20:24:08 +0200

    refactoring

--- assets/js/bin/app.init.js
+++ assets/js/bin/app.init.js
@@ -1,13 +1,10 @@
 $('document').ready(function () {
 
+    $('.parallax').parallax();
     $('.button-collapse').sideNav({
         menuWidth: 300,
         edge: 'left',
     });
-
-    let select = document.querySelectorAll('select');
-    for(let i = 0; i < select.length; i++) {
-        $(select[i]).material_select();
-    }
+    $('select').material_select();
 
 })
