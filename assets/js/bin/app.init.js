1518258ef904e6691d930133a6c23017b32528f6
Gaelly <gaellynkulu@gmail.com>
2018-07-30 15:24:20 +0200

    Amélioration de la page account_producteur

--- assets/js/bin/app.init.js
+++ assets/js/bin/app.init.js
@@ -7,4 +7,6 @@ $('document').ready(function () {
     $('select').material_select();
 
 })
-$('.carousel.carousel-slider').carousel({ fullWidth: true });
+ $(document).ready(function () {
+     $('.carousel').carousel();
+ });
